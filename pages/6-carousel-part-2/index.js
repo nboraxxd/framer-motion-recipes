78f17f8813eca6e35e4696d2c68e3a8959528f77
bora <nhatbaoxxd@gmail.com>
2023-11-20 22:13:11 +0700

    Carousel part 2 (final)

--- pages/6-carousel-part-2/index.js
+++ pages/6-carousel-part-2/index.js
@@ -1,54 +1,46 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
-import { useState } from "react";
-import { AnimatePresence, motion, MotionConfig } from "framer-motion";
-import useKeypress from "react-use-keypress";
+import { useState } from 'react'
+import { AnimatePresence, MotionConfig, motion } from 'framer-motion'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import useKeypress from 'react-use-keypress'
 
-let images = [
-  "/images/1.jpeg",
-  "/images/2.jpeg",
-  "/images/3.jpeg",
-  "/images/4.jpeg",
-  "/images/5.jpeg",
-  "/images/6.jpeg",
-  "/images/1.jpeg?1",
-  "/images/2.jpeg?1",
-  "/images/3.jpeg?1",
-  "/images/4.jpeg?1",
-  "/images/5.jpeg?1",
-  "/images/6.jpeg?1",
-  "/images/1.jpeg?2",
-  "/images/2.jpeg?2",
-  "/images/3.jpeg?2",
-  "/images/4.jpeg?2",
-  "/images/5.jpeg?2",
-  "/images/6.jpeg?2",
-  "/images/1.jpeg?3",
-  "/images/2.jpeg?3",
-  "/images/3.jpeg?3",
-  "/images/4.jpeg?3",
-  "/images/5.jpeg?3",
-  "/images/6.jpeg?3",
-];
+const images = [
+  '/images/1.jpeg',
+  '/images/2.jpeg',
+  '/images/3.jpeg',
+  '/images/4.jpeg',
+  '/images/5.jpeg',
+  '/images/6.jpeg',
+  '/images/1.jpeg?1',
+  '/images/2.jpeg?1',
+  '/images/3.jpeg?1',
+  '/images/4.jpeg?1',
+  '/images/5.jpeg?1',
+  '/images/6.jpeg?1',
+  '/images/1.jpeg?2',
+  '/images/2.jpeg?2',
+  '/images/3.jpeg?2',
+  '/images/4.jpeg?2',
+  '/images/5.jpeg?2',
+  '/images/6.jpeg?2',
+]
 
-let collapsedAspectRatio = 1 / 3;
-let fullAspectRatio = 3 / 2;
-let margin = 12;
-let gap = 2;
+const collapsedAspectRatio = 1 / 3
+const fullAspectRatio = 3 / 2
+const margin = 10
+const gap = 2
 
 export default function Page() {
-  let [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0)
 
-  useKeypress("ArrowRight", () => {
-    if (index < images.length - 1) {
-      setIndex(index + 1);
-    }
-  });
+  useKeypress('ArrowRight', () => {
+    if (index >= images.length - 1) return
+    setIndex(index + 1)
+  })
 
-  useKeypress("ArrowLeft", () => {
-    if (index > 0) {
-      setIndex(index - 1);
-    }
-  });
+  useKeypress('ArrowLeft', () => {
+    if (index <= 0) return
+    setIndex(index - 1)
+  })
 
   return (
     <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
@@ -58,9 +50,9 @@ export default function Page() {
             <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
               {images.map((image, i) => (
                 <motion.img
+                  animate={{ opacity: i === index ? 1 : 0.3 }}
                   key={image}
                   src={image}
-                  animate={{ opacity: i === index ? 1 : 0.3 }}
                   className="aspect-[3/2] object-cover"
                 />
               ))}
@@ -70,10 +62,13 @@ export default function Page() {
                 <motion.button
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 0.7 }}
-                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  exit={{ opacity: 0, pointerEvents: 'none' }}
                   whileHover={{ opacity: 1 }}
-                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
-                  onClick={() => setIndex(index - 1)}
+                  className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/80"
+                  onClick={() => {
+                    console.log(index - 1)
+                    setIndex(index - 1)
+                  }}
                 >
                   <ChevronLeftIcon className="h-6 w-6" />
                 </motion.button>
@@ -85,9 +80,9 @@ export default function Page() {
                 <motion.button
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 0.7 }}
-                  exit={{ opacity: 0, pointerEvents: "none" }}
+                  exit={{ opacity: 0, pointerEvents: 'none' }}
                   whileHover={{ opacity: 1 }}
-                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                  className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/80"
                   onClick={() => setIndex(index + 1)}
                 >
                   <ChevronRightIcon className="h-6 w-6" />
@@ -99,25 +94,14 @@ export default function Page() {
           <div className="absolute inset-x-0 bottom-6 flex h-14 justify-center overflow-hidden">
             <motion.div
               initial={false}
-              animate={{
-                x: `-${
-                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
-                  margin +
-                  index * gap
-                }%`,
-              }}
-              style={{
-                aspectRatio: fullAspectRatio,
-                gap: `${gap}%`,
-              }}
+              animate={{ x: `-${index * 100 * (collapsedAspectRatio / fullAspectRatio) + margin + index * gap}%` }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
               className="flex"
             >
               {images.map((image, i) => (
                 <motion.button
+                  key={image}
                   onClick={() => setIndex(i)}
-                  initial={false}
-                  whileHover={{ opacity: 1 }}
-                  animate={i === index ? "active" : "inactive"}
                   variants={{
                     active: {
                       aspectRatio: fullAspectRatio,
@@ -132,10 +116,13 @@ export default function Page() {
                       opacity: 0.5,
                     },
                   }}
+                  initial={false}
+                  animate={i === index ? 'active' : 'inactive'}
+                  whileHover={{ opacity: 1 }}
+                  transition={{ ease: [0.25, 0.5, 0.75, 1] }}
                   className="shrink-0"
-                  key={image}
                 >
-                  <img src={image} className="h-full object-cover" />
+                  <img src={image} className="h-full rounded-sm object-cover" />
                 </motion.button>
               ))}
             </motion.div>
@@ -143,5 +130,5 @@ export default function Page() {
         </div>
       </div>
     </MotionConfig>
-  );
+  )
 }
